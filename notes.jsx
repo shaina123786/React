ad86de99072be215bf1228e20316ad9afda3f333
Shaina Nawaz <nawazshaina4@gmail.com>
2025-07-27 10:16:11 +0530

    REACT

---
+++ notes.jsx
@@ -0,0 +1,5 @@
+//CDN(context delivery network)->react(libraries) has been hosted there we r take nd use it on our project
+//cross origin
+//npm is manage package
+// package.json is configuration for npm
+// dependency->dev and normal
